94fb8ec37bf5ff974f29efce2a12bef5b615824e
CodinGitHub <gms2programador@gmail.com>
2021-07-28 03:53:54 -0500

    Fonto color changed

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let mostraFecha = document.getElementById('fecha');
 
 let fecha = new Date();
 
-let diaSemana = ['Domingo','Lunes', 'Martes','Miercoles','Jueves','Viernes','Sabado'];
+let diaSemana = ['Domingo','Lunes', 'Martes','Miércoles','Jueves','Viernes','Sábado'];
 
 let mesAnyo = ['Enero','Febrero', 'Marzo','abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
 
